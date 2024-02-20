ad842625bfde5ead86fa4eb25fb70f40b6dda416
Matej Hlatký <hlatky@freevision.sk>
2024-02-20 15:45:29 +0100

    Fix Android setup for eidmsdk_flutter

--- android/app/src/main/kotlin/digital/slovensko/autogram/MainActivity.kt
+++ android/app/src/main/kotlin/digital/slovensko/autogram/MainActivity.kt
@@ -1,6 +1,6 @@
 package digital.slovensko.autogram
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity: FlutterActivity() {
+class MainActivity: FlutterFragmentActivity() {
 }
