d10fbc886dd4d03d7263fd6afa83768030f3fd69
Ahmed Al Hafoudh <alhafoudh@freevision.sk>
2024-02-23 21:17:04 +0100

    Fix dataToSign encoding

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:logging/logging.dart';
 
 import 'app.dart';
 import 'di.dart';
 
 void main() {
   configureDependencies();
+  Logger.root.level = Level.ALL; // defaults to Level.INFO
+  Logger.root.onRecord.listen((record) {
+    print('${record.level.name}: ${record.time}: ${record.message}');
+  });
   runApp(const App());
 }
