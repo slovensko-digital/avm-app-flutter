dcdd28916abea04fcf74ffd87a03118f67b30a23
Matej Hlatký <hlatky@freevision.sk>
2025-07-01 21:57:05 +0200

    Update Flutter to v3.27.4

--- lib/l10n/app_localizations_sk.dart
+++ lib/l10n/app_localizations_sk.dart
@@ -2,6 +2,8 @@ import 'package:intl/intl.dart' as intl;
 
 import 'app_localizations.dart';
 
+// ignore_for_file: type=lint
+
 /// The translations for Slovak (`sk`).
 class AppLocalizationsSk extends AppLocalizations {
   AppLocalizationsSk([String locale = 'sk']) : super(locale);
