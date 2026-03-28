slovensko-digital/avm-app-flutter

commit 4badea47808e38d71ba9f1f3dd5884b45610ce2c

Po podpise z URL automaticky zavrieť AVM (#107)
* Passing flag whether the flow is from DeepLink and  closing app after successful signing of the remote file
* Add missing SafeArea

lib/ui/screens/present_signed_document_screen.dart
@@ -4,6 +4,7 @@ import 'dart:io' show File, OSError, PathAccessException;
 import 'package:autogram_sign/autogram_sign.dart' show SignDocumentResponseBody;
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart' show SystemNavigator;
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:share_plus/share_plus.dart';
 import 'package:widgetbook/widgetbook.dart';
@@ -29,11 +30,13 @@ import '../widgets/result_view.dart';
 class PresentSignedDocumentScreen extends StatelessWidget {
   final SignDocumentResponseBody signedDocument;
   final DocumentSigningType signingType;
+  final bool openFromDeepLink;
 
   const PresentSignedDocumentScreen({
     super.key,
     required this.signedDocument,
     required this.signingType,
+    required this.openFromDeepLink,
   });
 
   @override
@@ -55,30 +58,32 @@ class PresentSignedDocumentScreen extends StatelessWidget {
         appBar: AppBar(
           automaticallyImplyLeading: false,
         ),
-        body: Builder(
-          builder: (context) {
-            // Need outer Context to access Cubit
-            return BlocConsumer<PresentSignedDocumentCubit,
-                PresentSignedDocumentState>(
-              listener: (context, state) {
-                if (state is PresentSignedDocumentErrorState) {
-                  final error = state.error;
-                  final message = context.strings
-                      .saveSignedDocumentErrorMessage(getErrorMessage(error));
+        body: SafeArea(
+          child: Builder(
+            builder: (context) {
+              // Need outer Context to access Cubit
+              return BlocConsumer<PresentSignedDocumentCubit,
+                  PresentSignedDocumentState>(
+                listener: (context, state) {
+                  if (state is PresentSignedDocumentErrorState) {
+                    final error = state.error;
+                    final message = context.strings
+                        .saveSignedDocumentErrorMessage(getErrorMessage(error));
 
-                  _showError(context, message);
-                }
-              },
-              builder: (context, state) {
-                return _Body(
-                  state: state,
-                  signingType: signingType,
-                  onShareFileRequested: () => _handleShareFile(context),
-                  onCloseRequested: () => _handleClose(context),
-                );
-              },
-            );
-          },
+                    _showError(context, message);
+                  }
+                },
+                builder: (context, state) {
+                  return _Body(
+                    state: state,
+                    signingType: signingType,
+                    onShareFileRequested: () => _handleShareFile(context),
+                    onCloseRequested: () => _handleClose(context),
+                  );
+                },
+              );
+            },
+          ),
         ),
       ),
     );
@@ -127,7 +132,11 @@ class PresentSignedDocumentScreen extends StatelessWidget {
 
   /// Handles close request.
   Future<void> _handleClose(BuildContext context) {
-    return Navigator.of(context).maybePop();
+    if (openFromDeepLink) {
+      return SystemNavigator.pop();
+    } else {
+      return Navigator.of(context).maybePop();
+    }
   }
 }
 

lib/l10n/app_localizations_sk.dart
@@ -1,5 +1,5 @@
+// ignore: unused_import
 import 'package:intl/intl.dart' as intl;
-
 import 'app_localizations.dart';
 
 // ignore_for_file: type=lint

lib/ui/screens/about_screen.dart
@@ -25,7 +25,7 @@ class AboutScreen extends StatelessWidget {
         title: Text(context.strings.aboutTitle),
         actions: const [CloseButton()],
       ),
-      body: const _Body(),
+      body: const SafeArea(child: _Body()),
     );
   }
 }

lib/ui/screens/main_screen.dart
@@ -183,6 +183,7 @@ class _MainScreenState extends State<MainScreen> {
       final screen = PreviewDocumentScreen(
         documentId: action.guid,
         file: null,
+        openFromDeepLink: true,
       );
       final route = MaterialPageRoute(builder: (_) => screen);
 

lib/ui/screens/open_document_screen.dart
@@ -64,9 +64,7 @@ class OpenDocumentScreen extends StatelessWidget {
       file: state.file,
       documentId: state.documentId,
     );
-    final route = MaterialPageRoute(
-      builder: (context) => screen,
-    );
+    final route = MaterialPageRoute(builder: (context) => screen);
 
     Navigator.of(context).pushReplacement(route);
   }

lib/ui/screens/preview_document_screen.dart
@@ -35,11 +35,13 @@ import 'select_certificate_screen.dart';
 class PreviewDocumentScreen extends StatelessWidget {
   final File? file;
   final String documentId;
+  final bool openFromDeepLink;
 
   const PreviewDocumentScreen({
     super.key,
     this.file,
     required this.documentId,
+    this.openFromDeepLink = false,
   });
 
   @override
@@ -90,9 +92,7 @@ class PreviewDocumentScreen extends StatelessWidget {
             ),
         ],
       ),
-      body: SafeArea(
-        child: body,
-      ),
+      body: SafeArea(child: body),
     );
   }
 
@@ -128,10 +128,9 @@ class PreviewDocumentScreen extends StatelessWidget {
       documentId: documentId,
       signingType:
           file != null ? DocumentSigningType.local : DocumentSigningType.remote,
+      openFromDeepLink: openFromDeepLink,
     );
-    final route = MaterialPageRoute(
-      builder: (context) => screen,
-    );
+    final route = MaterialPageRoute(builder: (context) => screen);
 
     return Navigator.of(context).push(route);
   }

lib/ui/screens/select_certificate_screen.dart
@@ -31,11 +31,13 @@ import 'sign_document_screen.dart';
 class SelectCertificateScreen extends StatelessWidget {
   final String documentId;
   final DocumentSigningType signingType;
+  final bool openFromDeepLink;
 
   const SelectCertificateScreen({
     super.key,
     required this.documentId,
     required this.signingType,
+    required this.openFromDeepLink,
   });
 
   @override
@@ -61,20 +63,22 @@ class SelectCertificateScreen extends StatelessWidget {
                   ? Text(context.strings.selectCertificateTitle)
                   : null,
             ),
-            body: _Body(
-              state: state,
-              signingType: signingType,
-              documentId: documentId,
-              onSignDocumentRequested: (certificate, signatureType) {
-                _onSignDocumentRequested(
-                  context: context,
-                  certificate: certificate,
-                  signatureType: signatureType,
-                );
-              },
-              onReloadCertificatesRequested: () {
-                _onReloadCertificatesRequested(context);
-              },
+            body: SafeArea(
+              child: _Body(
+                state: state,
+                signingType: signingType,
+                documentId: documentId,
+                onSignDocumentRequested: (certificate, signatureType) {
+                  _onSignDocumentRequested(
+                    context: context,
+                    certificate: certificate,
+                    signatureType: signatureType,
+                  );
+                },
+                onReloadCertificatesRequested: () {
+                  _onReloadCertificatesRequested(context);
+                },
+              ),
             ),
           );
         },
@@ -94,6 +98,7 @@ class SelectCertificateScreen extends StatelessWidget {
       certificate: certificate,
       signatureType: signatureType,
       signingType: signingType,
+      openFromDeepLink: openFromDeepLink,
     );
     final route = MaterialPageRoute(builder: (_) => screen);
 

lib/ui/screens/show_document_screen.dart
@@ -24,17 +24,15 @@ class ShowDocumentScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final body = ShowWebPageFragment(
-      url: url,
-    );
-
     return Scaffold(
       appBar: AppBar(
         automaticallyImplyLeading: false,
         title: Text(title),
         actions: const [CloseButton()],
       ),
-      body: body,
+      body: SafeArea(
+        child: ShowWebPageFragment(url: url),
+      ),
     );
   }
 }

commit c3ae67e9fe6953c774837e0087df5ef61ce679d1

Fix issue caused by #107
SystemNavigator.pop is no-op on iOS

lib/ui/screens/present_signed_document_screen.dart
@@ -1,5 +1,5 @@
 import 'dart:developer' as developer;
-import 'dart:io' show File, OSError, PathAccessException;
+import 'dart:io' show File, OSError, PathAccessException, Platform;
 
 import 'package:autogram_sign/autogram_sign.dart' show SignDocumentResponseBody;
 import 'package:flutter/foundation.dart';
@@ -132,8 +132,7 @@ class PresentSignedDocumentScreen extends StatelessWidget {
 
   /// Handles close request.
   Future<void> _handleClose(BuildContext context) {
-    if (openFromDeepLink) {
-      // TODO Fix for IOS
+    if (openFromDeepLink && Platform.isAndroid) {
       return SystemNavigator.pop();
     } else {
       return Navigator.of(context).maybePop();