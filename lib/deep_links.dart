39b92d9d7107b6dc67ad9bb490c61d3d53af9fb2
Matej Hlatký <hlatky@freevision.sk>
2026-03-28 18:49:59 +0100

    Add TODOs

--- lib/deep_links.dart
+++ lib/deep_links.dart
@@ -6,7 +6,7 @@
 ///  - path: "/api/v1/"
 ///
 /// Throws [ArgumentError] in case of invalid or unknown schema or structure.
-// TODO Move this code + test into autogram_sign module; however, it needs to check uri.authority, so it should be function on IAutogramService
+// TODO PAIRING Move this code + test into autogram_sign module; however, it needs to check uri.authority, so it should be function on IAutogramService
 DeepLinkAction parseDeepLinkAction(Uri uri) {
   // Validate schema, authority and path
   switch (uri.scheme) {
@@ -14,52 +14,76 @@ DeepLinkAction parseDeepLinkAction(Uri uri) {
       break;
     default:
       throw ArgumentError.value(
-          uri.toString(), "uri", "Invalid or unsupported scheme.");
+        uri.toString(),
+        "uri",
+        "Invalid or unsupported scheme.",
+      );
   }
 
+  // TODO PAIRING Check authority dynamically from baseUrl
   if (uri.authority != "autogram.slovensko.digital") {
     throw ArgumentError.value(
-        uri.toString(), "uri", "Invalid or unsupported authority.");
+      uri.toString(),
+      "uri",
+      "Invalid or unsupported authority.",
+    );
   }
 
   if (!uri.path.startsWith("/api/v1/")) {
     throw ArgumentError.value(
-        uri.toString(), "uri", "Invalid or unsupported path.");
+      uri.toString(),
+      "uri",
+      "Invalid or unsupported path.",
+    );
   }
+
+  // https://autogram.slovensko.digital/api/v1/qr-code-register?integration=eyJhbGciOiJFUzI1NiJ9.eyJqdGkiOiI3ZTBjMTBkNC1kY2FmLTRhNTYtYjA0YS1jMmIzNzc0ODM1YjciLCJzdWIiOiIxMTdhMGIwNC1mM2Q0LTQ5OWMtYTYxMy02YzY1MmU0ODZmMDEiLCJhdWQiOiJkZXZpY2UiLCJleHAiOjE3NzQ3MTg1Nzl9.tkc5qJwpEz2LPRnmhZVEPc_FM_iBRduinfIjUr8OzE1kgMJfuEZ8Kh4M2DGPXzSF-x5SgR5wXsXw_ZmW210zYA
 
   return switch (uri.path) {
     "/api/v1/qr-code" => () {
-        if (!uri.queryParameters.containsKey("guid")) {
-          throw ArgumentError.value(
-              uri.toString(), "uri", '"guid" param is missing a value.');
-        }
-
-        if (!uri.queryParameters.containsKey("key")) {
-          throw ArgumentError.value(
-              uri.toString(), "uri", '"key" param is missing a value.');
-        }
-
-        return SignRemoteDocumentAction(
-          guid: uri.queryParameters["guid"]!,
-          key: uri.queryParameters["key"]!,
-          integration: uri.queryParameters["integration"],
-          pushkey: uri.queryParameters["pushkey"],
+      if (!uri.queryParameters.containsKey("guid")) {
+        throw ArgumentError.value(
+          uri.toString(),
+          "uri",
+          '"guid" param is missing a value.',
         );
-      }(),
+      }
+
+      if (!uri.queryParameters.containsKey("key")) {
+        throw ArgumentError.value(
+          uri.toString(),
+          "uri",
+          '"key" param is missing a value.',
+        );
+      }
+
+      return SignRemoteDocumentAction(
+        guid: uri.queryParameters["guid"]!,
+        key: uri.queryParameters["key"]!,
+        integration: uri.queryParameters["integration"],
+        pushkey: uri.queryParameters["pushkey"],
+      );
+    }(),
     _ => throw ArgumentError.value(
-        uri.toString(), "uri", "Invalid or unsupported URI."),
+      uri.toString(),
+      "uri",
+      "Invalid or unsupported URI.",
+    ),
   };
 }
 
 /// Supported Deep Link action.
 sealed class DeepLinkAction {}
 
+// TODO PAIRING Create type to RegisterIntegrationAction
+// TODO PAIRING Create interface for IntegrationAction
+
 /// Action to sign remote document.
 class SignRemoteDocumentAction extends DeepLinkAction {
   final String guid;
   final String key;
   final String? integration;
-  final String? pushkey;
+  final String? pushkey; // TODO PAIRING Drop pushkey, but keep integration
 
   SignRemoteDocumentAction({
     required this.guid,
