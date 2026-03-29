slovensko-digital/avm-app-flutter

commit 4badea47808e38d71ba9f1f3dd5884b45610ce2c

Po podpise z URL automaticky zavrieť AVM (#107)
* Passing flag whether the flow is from DeepLink and  closing app after successful signing of the remote file
* Add missing SafeArea

lib/ui/screens/present_signed_document_screen.dart
@@ -4,6 +4,7 @@ import 'dart:io' show File, OSError, PathAccessException;
 import 'package:autogram_sign/autogram_sign.dart' show SignDocumentResponseBody;
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart' show SystemNavigator;
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:share_plus/share_plus.dart';
 import 'package:widgetbook/widgetbook.dart';
@@ -29,11 +30,13 @@ import '../widgets/result_view.dart';
 class PresentSignedDocumentScreen extends StatelessWidget {
   final SignDocumentResponseBody signedDocument;
   final DocumentSigningType signingType;
+  final bool openFromDeepLink;
 
   const PresentSignedDocumentScreen({
     super.key,
     required this.signedDocument,
     required this.signingType,
+    required this.openFromDeepLink,
   });
 
   @override
@@ -55,30 +58,32 @@ class PresentSignedDocumentScreen extends StatelessWidget {
         appBar: AppBar(
           automaticallyImplyLeading: false,
         ),
-        body: Builder(
-          builder: (context) {
-            // Need outer Context to access Cubit
-            return BlocConsumer<PresentSignedDocumentCubit,
-                PresentSignedDocumentState>(
-              listener: (context, state) {
-                if (state is PresentSignedDocumentErrorState) {
-                  final error = state.error;
-                  final message = context.strings
-                      .saveSignedDocumentErrorMessage(getErrorMessage(error));
+        body: SafeArea(
+          child: Builder(
+            builder: (context) {
+              // Need outer Context to access Cubit
+              return BlocConsumer<PresentSignedDocumentCubit,
+                  PresentSignedDocumentState>(
+                listener: (context, state) {
+                  if (state is PresentSignedDocumentErrorState) {
+                    final error = state.error;
+                    final message = context.strings
+                        .saveSignedDocumentErrorMessage(getErrorMessage(error));
 
-                  _showError(context, message);
-                }
-              },
-              builder: (context, state) {
-                return _Body(
-                  state: state,
-                  signingType: signingType,
-                  onShareFileRequested: () => _handleShareFile(context),
-                  onCloseRequested: () => _handleClose(context),
-                );
-              },
-            );
-          },
+                    _showError(context, message);
+                  }
+                },
+                builder: (context, state) {
+                  return _Body(
+                    state: state,
+                    signingType: signingType,
+                    onShareFileRequested: () => _handleShareFile(context),
+                    onCloseRequested: () => _handleClose(context),
+                  );
+                },
+              );
+            },
+          ),
         ),
       ),
     );
@@ -127,7 +132,11 @@ class PresentSignedDocumentScreen extends StatelessWidget {
 
   /// Handles close request.
   Future<void> _handleClose(BuildContext context) {
-    return Navigator.of(context).maybePop();
+    if (openFromDeepLink) {
+      return SystemNavigator.pop();
+    } else {
+      return Navigator.of(context).maybePop();
+    }
   }
 }
 

lib/l10n/app_localizations_sk.dart
@@ -1,5 +1,5 @@
+// ignore: unused_import
 import 'package:intl/intl.dart' as intl;
-
 import 'app_localizations.dart';
 
 // ignore_for_file: type=lint

lib/ui/screens/about_screen.dart
@@ -25,7 +25,7 @@ class AboutScreen extends StatelessWidget {
         title: Text(context.strings.aboutTitle),
         actions: const [CloseButton()],
       ),
-      body: const _Body(),
+      body: const SafeArea(child: _Body()),
     );
   }
 }

lib/ui/screens/main_screen.dart
@@ -183,6 +183,7 @@ class _MainScreenState extends State<MainScreen> {
       final screen = PreviewDocumentScreen(
         documentId: action.guid,
         file: null,
+        openFromDeepLink: true,
       );
       final route = MaterialPageRoute(builder: (_) => screen);
 

lib/ui/screens/open_document_screen.dart
@@ -64,9 +64,7 @@ class OpenDocumentScreen extends StatelessWidget {
       file: state.file,
       documentId: state.documentId,
     );
-    final route = MaterialPageRoute(
-      builder: (context) => screen,
-    );
+    final route = MaterialPageRoute(builder: (context) => screen);
 
     Navigator.of(context).pushReplacement(route);
   }

lib/ui/screens/preview_document_screen.dart
@@ -35,11 +35,13 @@ import 'select_certificate_screen.dart';
 class PreviewDocumentScreen extends StatelessWidget {
   final File? file;
   final String documentId;
+  final bool openFromDeepLink;
 
   const PreviewDocumentScreen({
     super.key,
     this.file,
     required this.documentId,
+    this.openFromDeepLink = false,
   });
 
   @override
@@ -90,9 +92,7 @@ class PreviewDocumentScreen extends StatelessWidget {
             ),
         ],
       ),
-      body: SafeArea(
-        child: body,
-      ),
+      body: SafeArea(child: body),
     );
   }
 
@@ -128,10 +128,9 @@ class PreviewDocumentScreen extends StatelessWidget {
       documentId: documentId,
       signingType:
           file != null ? DocumentSigningType.local : DocumentSigningType.remote,
+      openFromDeepLink: openFromDeepLink,
     );
-    final route = MaterialPageRoute(
-      builder: (context) => screen,
-    );
+    final route = MaterialPageRoute(builder: (context) => screen);
 
     return Navigator.of(context).push(route);
   }

lib/ui/screens/select_certificate_screen.dart
@@ -31,11 +31,13 @@ import 'sign_document_screen.dart';
 class SelectCertificateScreen extends StatelessWidget {
   final String documentId;
   final DocumentSigningType signingType;
+  final bool openFromDeepLink;
 
   const SelectCertificateScreen({
     super.key,
     required this.documentId,
     required this.signingType,
+    required this.openFromDeepLink,
   });
 
   @override
@@ -61,20 +63,22 @@ class SelectCertificateScreen extends StatelessWidget {
                   ? Text(context.strings.selectCertificateTitle)
                   : null,
             ),
-            body: _Body(
-              state: state,
-              signingType: signingType,
-              documentId: documentId,
-              onSignDocumentRequested: (certificate, signatureType) {
-                _onSignDocumentRequested(
-                  context: context,
-                  certificate: certificate,
-                  signatureType: signatureType,
-                );
-              },
-              onReloadCertificatesRequested: () {
-                _onReloadCertificatesRequested(context);
-              },
+            body: SafeArea(
+              child: _Body(
+                state: state,
+                signingType: signingType,
+                documentId: documentId,
+                onSignDocumentRequested: (certificate, signatureType) {
+                  _onSignDocumentRequested(
+                    context: context,
+                    certificate: certificate,
+                    signatureType: signatureType,
+                  );
+                },
+                onReloadCertificatesRequested: () {
+                  _onReloadCertificatesRequested(context);
+                },
+              ),
             ),
           );
         },
@@ -94,6 +98,7 @@ class SelectCertificateScreen extends StatelessWidget {
       certificate: certificate,
       signatureType: signatureType,
       signingType: signingType,
+      openFromDeepLink: openFromDeepLink,
     );
     final route = MaterialPageRoute(builder: (_) => screen);
 

lib/ui/screens/show_document_screen.dart
@@ -24,17 +24,15 @@ class ShowDocumentScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final body = ShowWebPageFragment(
-      url: url,
-    );
-
     return Scaffold(
       appBar: AppBar(
         automaticallyImplyLeading: false,
         title: Text(title),
         actions: const [CloseButton()],
       ),
-      body: body,
+      body: SafeArea(
+        child: ShowWebPageFragment(url: url),
+      ),
     );
   }
 }

commit 4384d162ed9bc0d745a678e76611207b8e3f1e1a

eID mSDK update v2.0 (#113)
* Change iOS Deployment target to 14.0
* Update Firebase Core still compatible with iOS 14
* Fix some warnings from recent Flutter update
* Fix issue caused by #107

ios/Flutter/AppFrameworkInfo.plist
@@ -21,6 +21,6 @@
   <key>CFBundleVersion</key>
   <string>1.0</string>
   <key>MinimumOSVersion</key>
-  <string>13.0</string>
+  <string>14.0</string>
 </dict>
 </plist>

ios/Podfile
@@ -1,4 +1,4 @@
-platform :ios, '13.0'
+platform :ios, '14.0'
 
 # CocoaPods analytics sends network stats synchronously affecting flutter build latency.
 ENV['COCOAPODS_DISABLE_STATS'] = 'true'
@@ -29,7 +29,6 @@ flutter_ios_podfile_setup
 target 'Runner' do
   use_frameworks!
   use_modular_headers!
-  pod 'JWTDecode', '3.1.0'
 
   flutter_install_all_ios_pods File.dirname(File.realpath(__FILE__))
   target 'RunnerTests' do
@@ -41,7 +40,7 @@ post_install do |installer|
   installer.pods_project.targets.each do |target|
     flutter_additional_ios_build_settings(target)
     target.build_configurations.each do |config|
-      config.build_settings['IPHONEOS_DEPLOYMENT_TARGET'] = '13.0'
+      config.build_settings['IPHONEOS_DEPLOYMENT_TARGET'] = '14.0'
     end
   end
 end

ios/Runner.xcodeproj/project.pbxproj
@@ -481,7 +481,7 @@
 				GCC_WARN_UNINITIALIZED_AUTOS = YES_AGGRESSIVE;
 				GCC_WARN_UNUSED_FUNCTION = YES;
 				GCC_WARN_UNUSED_VARIABLE = YES;
-				IPHONEOS_DEPLOYMENT_TARGET = 13.0;
+				IPHONEOS_DEPLOYMENT_TARGET = 14.0;
 				MTL_ENABLE_DEBUG_INFO = NO;
 				SDKROOT = iphoneos;
 				SUPPORTED_PLATFORMS = iphoneos;
@@ -505,7 +505,7 @@
 				ENABLE_BITCODE = NO;
 				INFOPLIST_FILE = Runner/Info.plist;
 				INFOPLIST_KEY_CFBundleDisplayName = "Autogram v mobile";
-				IPHONEOS_DEPLOYMENT_TARGET = 13.0;
+				IPHONEOS_DEPLOYMENT_TARGET = 14.0;
 				LD_RUNPATH_SEARCH_PATHS = (
 					"$(inherited)",
 					"@executable_path/Frameworks",
@@ -619,7 +619,7 @@
 				GCC_WARN_UNINITIALIZED_AUTOS = YES_AGGRESSIVE;
 				GCC_WARN_UNUSED_FUNCTION = YES;
 				GCC_WARN_UNUSED_VARIABLE = YES;
-				IPHONEOS_DEPLOYMENT_TARGET = 13.0;
+				IPHONEOS_DEPLOYMENT_TARGET = 14.0;
 				MTL_ENABLE_DEBUG_INFO = YES;
 				ONLY_ACTIVE_ARCH = YES;
 				SDKROOT = iphoneos;
@@ -668,7 +668,7 @@
 				GCC_WARN_UNINITIALIZED_AUTOS = YES_AGGRESSIVE;
 				GCC_WARN_UNUSED_FUNCTION = YES;
 				GCC_WARN_UNUSED_VARIABLE = YES;
-				IPHONEOS_DEPLOYMENT_TARGET = 13.0;
+				IPHONEOS_DEPLOYMENT_TARGET = 14.0;
 				MTL_ENABLE_DEBUG_INFO = NO;
 				SDKROOT = iphoneos;
 				SUPPORTED_PLATFORMS = iphoneos;
@@ -694,7 +694,7 @@
 				ENABLE_BITCODE = NO;
 				INFOPLIST_FILE = Runner/Info.plist;
 				INFOPLIST_KEY_CFBundleDisplayName = "Autogram v mobile";
-				IPHONEOS_DEPLOYMENT_TARGET = 13.0;
+				IPHONEOS_DEPLOYMENT_TARGET = 14.0;
 				LD_RUNPATH_SEARCH_PATHS = (
 					"$(inherited)",
 					"@executable_path/Frameworks",
@@ -727,7 +727,7 @@
 				ENABLE_BITCODE = NO;
 				INFOPLIST_FILE = Runner/Info.plist;
 				INFOPLIST_KEY_CFBundleDisplayName = "Autogram v mobile";
-				IPHONEOS_DEPLOYMENT_TARGET = 13.0;
+				IPHONEOS_DEPLOYMENT_TARGET = 14.0;
 				LD_RUNPATH_SEARCH_PATHS = (
 					"$(inherited)",
 					"@executable_path/Frameworks",

lib/app_service.dart
@@ -99,7 +99,7 @@ class AppService {
     _collectIncomingUri(uri);
   }
 
-  static _collectIncomingUri(dynamic value) {
+  static void _collectIncomingUri(dynamic value) {
     if (value is String && value.isNotEmpty) {
       final uri = Uri.tryParse(value);
 

lib/ui/screens/certificate_details_dialog.dart
@@ -25,9 +25,7 @@ class CertificateDetailsDialog extends StatelessWidget {
         title: null,
         actions: const [CloseButton()],
       ),
-      body: SafeArea(
-        child: body,
-      ),
+      body: SafeArea(child: body),
     );
   }
 
@@ -38,7 +36,7 @@ class CertificateDetailsDialog extends StatelessWidget {
     return showGeneralDialog(
       context: context,
       barrierDismissible: false,
-      pageBuilder: (context, __, ___) {
+      pageBuilder: (context, _, _) {
         return CertificateDetailsDialog(certificate);
       },
     );

lib/ui/screens/present_signed_document_screen.dart
@@ -1,5 +1,5 @@
 import 'dart:developer' as developer;
-import 'dart:io' show File, OSError, PathAccessException;
+import 'dart:io' show File, OSError, PathAccessException, Platform;
 
 import 'package:autogram_sign/autogram_sign.dart' show SignDocumentResponseBody;
 import 'package:flutter/foundation.dart';
@@ -55,15 +55,15 @@ class PresentSignedDocumentScreen extends StatelessWidget {
         return cubit;
       },
       child: Scaffold(
-        appBar: AppBar(
-          automaticallyImplyLeading: false,
-        ),
+        appBar: AppBar(automaticallyImplyLeading: false),
         body: SafeArea(
           child: Builder(
             builder: (context) {
               // Need outer Context to access Cubit
-              return BlocConsumer<PresentSignedDocumentCubit,
-                  PresentSignedDocumentState>(
+              return BlocConsumer<
+                PresentSignedDocumentCubit,
+                PresentSignedDocumentState
+              >(
                 listener: (context, state) {
                   if (state is PresentSignedDocumentErrorState) {
                     final error = state.error;
@@ -123,8 +123,9 @@ class PresentSignedDocumentScreen extends StatelessWidget {
       );
     } catch (error) {
       if (context.mounted) {
-        final message =
-            strings.shareSignedDocumentErrorMessage(getErrorMessage(error));
+        final message = strings.shareSignedDocumentErrorMessage(
+          getErrorMessage(error),
+        );
         _showError(context, message);
       }
     }
@@ -132,8 +133,7 @@ class PresentSignedDocumentScreen extends StatelessWidget {
 
   /// Handles close request.
   Future<void> _handleClose(BuildContext context) {
-    if (openFromDeepLink) {
-      // TODO Fix for IOS
+    if (openFromDeepLink && Platform.isAndroid) {
       return SystemNavigator.pop();
     } else {
       return Navigator.of(context).maybePop();
@@ -157,35 +157,33 @@ class _Body extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: kScreenMargin,
-      child: _getChild(context),
-    );
+    return Padding(padding: kScreenMargin, child: _getChild(context));
   }
 
   Widget _getChild(BuildContext context) {
     final sharingEnabled = (signingType == DocumentSigningType.local);
-    final onShareFileRequested =
-        sharingEnabled ? this.onShareFileRequested : null;
+    final onShareFileRequested = sharingEnabled
+        ? this.onShareFileRequested
+        : null;
 
     return switch (state) {
       PresentSignedDocumentInitialState _ => const LoadingContent(),
       PresentSignedDocumentLoadingState _ => const LoadingContent(),
       PresentSignedDocumentErrorState _ => _SuccessContent(
-          file: null,
-          onShareFileRequested: onShareFileRequested,
-          onCloseRequested: onCloseRequested,
-        ),
+        file: null,
+        onShareFileRequested: onShareFileRequested,
+        onCloseRequested: onCloseRequested,
+      ),
       PresentSignedLocalDocumentSuccessState state => _SuccessContent(
-          file: state.file,
-          onShareFileRequested: onShareFileRequested,
-          onCloseRequested: onCloseRequested,
-        ),
+        file: state.file,
+        onShareFileRequested: onShareFileRequested,
+        onCloseRequested: onCloseRequested,
+      ),
       PresentSignedRemoteDocumentSuccessState() => _SuccessContent(
-          file: null,
-          onShareFileRequested: null,
-          onCloseRequested: onCloseRequested,
-        ),
+        file: null,
+        onShareFileRequested: null,
+        onCloseRequested: onCloseRequested,
+      ),
     };
   }
 }
@@ -216,7 +214,7 @@ class _SuccessContent extends StatelessWidget {
 
       body = MarkdownText(
         text,
-        onLinkTap: (_, __, ___) {
+        onLinkTap: (_, _, _) {
           onShareFileRequested?.call();
         },
       );
@@ -245,9 +243,7 @@ class _SuccessContent extends StatelessWidget {
 
         // Secondary button
         TextButton(
-          style: TextButton.styleFrom(
-            minimumSize: kPrimaryButtonMinimumSize,
-          ),
+          style: TextButton.styleFrom(minimumSize: kPrimaryButtonMinimumSize),
           onPressed: onCloseRequested,
           child: Text(strings.buttonCloseLabel),
         ),
@@ -258,12 +254,12 @@ class _SuccessContent extends StatelessWidget {
   static String _getParentDirectoryName(File file) {
     return kIsWeb
         ? file.uri
-                .resolve('.')
-                .path
-                .split('/')
-                .where((e) => e.isNotEmpty)
-                .lastOrNull ??
-            '&nbsp;'
+                  .resolve('.')
+                  .path
+                  .split('/')
+                  .where((e) => e.isNotEmpty)
+                  .lastOrNull ??
+              '&nbsp;'
         : file.parent.basename;
   }
 }

lib/ui/widgets/chip.dart
@@ -32,9 +32,9 @@ class Chip extends StatelessWidget {
       ),
       padding: const EdgeInsets.only(left: 8, top: 6, bottom: 6, right: 12),
       child: Row(
+        spacing: 8,
         children: [
-          if (leading != null) leading!,
-          if (leading != null) const SizedBox(width: 8),
+          ?leading,
           Text(label, style: TextStyle(color: foreground)),
         ],
       ),
@@ -42,16 +42,9 @@ class Chip extends StatelessWidget {
   }
 }
 
-@widgetbook.UseCase(
-  path: '[AVM]',
-  name: '',
-  type: Chip,
-)
+@widgetbook.UseCase(path: '[AVM]', name: '', type: Chip)
 Widget previewChip(BuildContext context) {
-  var label = context.knobs.string(
-    label: 'Label',
-    initialValue: 'Chip!',
-  );
+  var label = context.knobs.string(label: 'Label', initialValue: 'Chip!');
   var foreground = context.knobs.color(
     label: 'Foreground',
     initialValue: material.Colors.black,

lib/ui/widgets/result_view.dart
@@ -31,23 +31,14 @@ class ResultView extends StatelessWidget {
     this.body,
   });
 
-  const ResultView.success({
-    super.key,
-    required this.titleText,
-    this.body,
-  }) : icon = 'assets/images/tick.svg';
+  const ResultView.success({super.key, required this.titleText, this.body})
+    : icon = 'assets/images/tick.svg';
 
-  const ResultView.error({
-    super.key,
-    required this.titleText,
-    this.body,
-  }) : icon = 'assets/images/danger.svg';
+  const ResultView.error({super.key, required this.titleText, this.body})
+    : icon = 'assets/images/danger.svg';
 
-  const ResultView.info({
-    super.key,
-    required this.titleText,
-    this.body,
-  }) : icon = 'assets/images/info.svg';
+  const ResultView.info({super.key, required this.titleText, this.body})
+    : icon = 'assets/images/info.svg';
 
   @override
   Widget build(BuildContext context) {
@@ -75,20 +66,12 @@ class ResultView extends StatelessWidget {
 
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        icon,
-        headline,
-        if (body != null) body!,
-      ],
+      children: [icon, headline, ?body],
     );
   }
 }
 
-@widgetbook.UseCase(
-  path: '[Core]',
-  name: 'info',
-  type: ResultView,
-)
+@widgetbook.UseCase(path: '[Core]', name: 'info', type: ResultView)
 Widget previewInfoResultView(BuildContext context) {
   final title = context.knobs.string(
     label: "Title",
@@ -102,18 +85,11 @@ Widget previewInfoResultView(BuildContext context) {
   );
 
   return Center(
-    child: ResultView.info(
-      titleText: title,
-      body: Text(body),
-    ),
+    child: ResultView.info(titleText: title, body: Text(body)),
   );
 }
 
-@widgetbook.UseCase(
-  path: '[Core]',
-  name: 'success',
-  type: ResultView,
-)
+@widgetbook.UseCase(path: '[Core]', name: 'success', type: ResultView)
 Widget previewSuccessResultView(BuildContext context) {
   final title = context.knobs.string(
     label: "Title",
@@ -127,18 +103,11 @@ Widget previewSuccessResultView(BuildContext context) {
   );
 
   return Center(
-    child: ResultView.success(
-      titleText: title,
-      body: Text(body),
-    ),
+    child: ResultView.success(titleText: title, body: Text(body)),
   );
 }
 
-@widgetbook.UseCase(
-  path: '[Core]',
-  name: 'error',
-  type: ResultView,
-)
+@widgetbook.UseCase(path: '[Core]', name: 'error', type: ResultView)
 Widget previewErrorResultView(BuildContext context) {
   final title = context.knobs.string(
     label: "Title",
@@ -152,18 +121,11 @@ Widget previewErrorResultView(BuildContext context) {
   );
 
   return Center(
-    child: ResultView.error(
-      titleText: title,
-      body: Text(body),
-    ),
+    child: ResultView.error(titleText: title, body: Text(body)),
   );
 }
 
-@widgetbook.UseCase(
-  path: '[Core]',
-  name: 'custom',
-  type: ResultView,
-)
+@widgetbook.UseCase(path: '[Core]', name: 'custom', type: ResultView)
 Widget previewCustomResultView(BuildContext context) {
   final title = context.knobs.string(
     label: "Title",
@@ -180,11 +142,7 @@ Widget previewCustomResultView(BuildContext context) {
     child: ResultView(
       icon: 'assets/images/notification.svg',
       titleText: title,
-      body: Column(
-        children: [
-          Text(body),
-        ],
-      ),
+      body: Column(children: [Text(body)]),
     ),
   );
 }

pubspec.yaml
@@ -1,7 +1,7 @@
 name: autogram
 description: "Autogram v mobile"
 publish_to: 'none' # Remove this line if you wish to publish to pub.dev
-version: 1.4.3+47
+version: 1.5.0+49
 
 environment:
   sdk: '>=3.10.9 <4.0.0'
@@ -14,7 +14,7 @@ dependencies:
   eidmsdk:
     git:
       url: https://github.com/slovensko-digital/eidmsdk-flutter
-      ref: v0.9.1
+      ref: v1.0.0
   autogram_sign:
     git:
       url: https://github.com/slovensko-digital/avm-client-dart
@@ -45,8 +45,8 @@ dependencies:
   notified_preferences:
   widgetbook_annotation:
   widgetbook:
-  firebase_core: 2.28.0
-  firebase_crashlytics: 3.5.0
+  firebase_core: 3.15.2
+  firebase_crashlytics:
   url_launcher:
   mobile_scanner: ^7.1.0
   html: